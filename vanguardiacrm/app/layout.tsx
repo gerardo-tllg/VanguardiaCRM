afdd4c7513f8f509126c36ce4e6133d60fa073a3
Gerardo <gerardo@fernandojlopez.com>
2026-04-21 14:00:46 -0500

    implementing docuent upload

--- vanguardiacrm/app/layout.tsx
+++ vanguardiacrm/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-// @ts-expect-error ignore globals error
+
 import "./globals.css";
 
 const geistSans = Geist({
