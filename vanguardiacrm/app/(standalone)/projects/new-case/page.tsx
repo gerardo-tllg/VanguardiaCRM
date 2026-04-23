3466ccd22dd766ecc3430e5f8ad31342abcac103
Gerardo <gerardo@fernandojlopez.com>
2026-04-23 15:19:18 -0500

    implementing docuent upload

--- vanguardiacrm/app/(standalone)/projects/new-case/page.tsx
+++ vanguardiacrm/app/(standalone)/projects/new-case/page.tsx
@@ -162,12 +162,7 @@ export default function NewCasePage() {
             <Link
               href="/projects"
               className="rounded-md border border-[#e5e5e5] bg-white px-5 py-2.5 text-sm font-medium text-[#2b2b2b] hover:bg-[#f7f7f7]"
-            ><Link
-  href="/projects"
-  className="rounded-md border border-[#e5e5e5] bg-white px-4 py-2 text-sm text-[#2b2b2b] hover:bg-[#f7f7f7]"
->
-  Back to Projects
-</Link>
+            >
               Cancel
             </Link>
           </div>
